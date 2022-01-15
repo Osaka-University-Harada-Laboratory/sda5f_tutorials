ed358d8de98f282c7c4bf1fe863fc90c7c9e44e5
takuya-ki <taku8926@gmail.com>
2022-01-15 12:08:22 +0000

    add initial files

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # sda5f_tutorials
+
+ROS package for YASKAWA MOTOMAN SDA5F tutorial.
+
+## Dependencies
+
+- ROS Kinetic
+- motoman driver
+
+## Installation
+
+    $ cd catkin_ws
+    $ git clone -b kinetic-devel https://github.com/ros-industrial/motoman.git src/motoman
+    $ git reset --hard $SHA1
+    $ git clone git@github.com:qqfly/motoman_sda5f_pkg.git src/motoman_sda5f_pkg
+    $ git clone git@github.com:Osaka-University-Harada-Laboratory/sda5f_tutorials.git src/sda5f_tutorials
+    $ rosdep update
+    $ rosdep install --from-paths src/ --ignore-src --rosdistro kinetic
+    $ cd catkin_ws; catkin build
+
+## Usage
+
+1. launch motoman driver  
+    `$ roslaunch motoman_sda5f_support robot_interface_streaming_sda5f.launch robot_ip:=X.X.X.X controller:=fs100`
+2. enable joint motors  
+    `$ rosservice call robot_enable`
+3. launch moveit  
+    `$ roslaunch motoman_sda5f_moveit_config demo.launch`
+4. execute tutorial demo  
+    `$ roslaunch sda5f_tutorials wiggle.launch`
+
+## Author / Contributor
+
+[Takuya Kiyokawa](https://takuya-ki.github.io/)
+
+## License
+
+This software is released under the MIT License, see [LICENSE](./LICENSE).
---
+++ scripts/wiggle.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+import math
+import rospy
+from geometry_msgs.msg import Pose, Quaternion, Vector3
+from moveit_commander import RobotCommander, MoveGroupCommander
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+
+
+def euler_to_quaternion(euler):
+    """Convert Euler Angles to Quaternion
+
+    euler: geometry_msgs/Vector3
+    quaternion: geometry_msgs/Quaternion
+    """
+    q = quaternion_from_euler(euler.x, euler.y, euler.z)
+    return Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
+
+
+def quaternion_to_euler(quaternion):
+    """Convert Quaternion to Euler Angles
+
+    quarternion: geometry_msgs/Quaternion
+    euler: geometry_msgs/Vector3
+    """
+    e = euler_from_quaternion((quaternion.x, quaternion.y, quaternion.z, quaternion.w))
+    return Vector3(x=e[0], y=e[1], z=e[2])
+
+
+def wiggle():
+    """Execute a wiggle motions
+
+    """
+    # get current status
+    rospy.init_node("moveit_command_sender")
+    robot = RobotCommander()
+    rarm = MoveGroupCommander("arm_right")
+    larm = MoveGroupCommander("arm_left")
+    rarm_initial_pose = rarm.get_current_pose().pose
+    rarm_initial_joint_values = rarm.get_current_joint_values()
+    larm_initial_pose = larm.get_current_pose().pose
+    larm_initial_joint_values = larm.get_current_joint_values()
+
+    # generate poses
+    for_pose_r = Pose()
+    for_pose_r.position.x = rarm_initial_pose.position.x + 0.1
+    for_pose_r.position.y = rarm_initial_pose.position.y
+    for_pose_r.position.z = rarm_initial_pose.position.z - 0.1
+    current_euler = quaternion_to_euler(rarm_initial_pose.orientation)
+    for_euler = Vector3()
+    for_euler.x = current_euler.x + math.radians(20.0) 
+    for_euler.y = current_euler.y 
+    for_euler.z = current_euler.z
+    for_pose_r.orientation = euler_to_quaternion(for_euler)
+
+    back_pose_r = Pose()
+    back_pose_r.position.x = rarm_initial_pose.position.x - 0.1
+    back_pose_r.position.y = rarm_initial_pose.position.y
+    back_pose_r.position.z = rarm_initial_pose.position.z - 0.1
+    current_euler = quaternion_to_euler(rarm_initial_pose.orientation)
+    back_euler = Vector3()
+    back_euler.x = current_euler.x + math.radians(-20.0) 
+    back_euler.y = current_euler.y 
+    back_euler.z = current_euler.z
+    back_pose_r.orientation = euler_to_quaternion(back_euler)
+
+    for_pose_l = Pose()
+    for_pose_l.position.x = larm_initial_pose.position.x + 0.1
+    for_pose_l.position.y = larm_initial_pose.position.y
+    for_pose_l.position.z = larm_initial_pose.position.z - 0.1
+    current_euler = quaternion_to_euler(larm_initial_pose.orientation)
+    for_euler = Vector3()
+    for_euler.x = current_euler.x + math.radians(-20.0) 
+    for_euler.y = current_euler.y 
+    for_euler.z = current_euler.z
+    for_pose_l.orientation = euler_to_quaternion(for_euler)
+
+    back_pose_l = Pose()
+    back_pose_l.position.x = larm_initial_pose.position.x - 0.1
+    back_pose_l.position.y = larm_initial_pose.position.y
+    back_pose_l.position.z = larm_initial_pose.position.z - 0.1
+    current_euler = quaternion_to_euler(larm_initial_pose.orientation)
+    back_euler = Vector3()
+    back_euler.x = current_euler.x + math.radians(20.0) 
+    back_euler.y = current_euler.y 
+    back_euler.z = current_euler.z
+    back_pose_l.orientation = euler_to_quaternion(back_euler)
+
+    # execute wiggle motions repeating 2 times for both arms
+    rospy.loginfo("Start right arm wiggle motions...")
+    cnt = 0
+    while cnt < 2:
+        rospy.loginfo("Right arm is moving...")
+        rarm.set_pose_target(for_pose_r)
+        rarm.go()
+        rospy.sleep(rospy.Duration.from_sec(1))
+        rarm.set_pose_target(back_pose_r)
+        rarm.go()
+        rospy.sleep(rospy.Duration.from_sec(1))
+        cnt += 1
+
+    rospy.loginfo("Start left arm wiggle motions...")
+    cnt = 0
+    while cnt < 2:
+        rospy.loginfo("Left arm is moving...")
+        larm.set_pose_target(for_pose_l)
+        larm.go()
+        rospy.sleep(rospy.Duration.from_sec(1))
+        larm.set_pose_target(back_pose_l)
+        larm.go()
+        rospy.sleep(rospy.Duration.from_sec(1))
+        cnt += 1
+
+    # initialize joints
+    rospy.loginfo("Initializing both arm joint values...")
+    rarm.set_joint_value_target(rarm_initial_joint_values)
+    rarm.go()
+    rospy.sleep(rospy.Duration.from_sec(1))
+    larm.set_joint_value_target(larm_initial_joint_values)
+    larm.go()
+    rospy.sleep(rospy.Duration.from_sec(1))
+    rospy.signal_shutdown("Finished.")
+
+
+if __name__ == '__main__':
+    try:
+        wiggle()
+    except rospy.ROSInterruptException:
+        pass
